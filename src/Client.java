ca2dae6bfafc6e886e7837cb824b8638a3426b05
lukey <ly105@student.le.ac.uk>
2024-02-18 00:04:04 +0000

    17/02/2024

--- src/Client.java
+++ src/Client.java
@@ -1,5 +1,38 @@
-public class Client {
-    public static void main(String[] args){
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Client {
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[1]); // port for server
+        String hostname = args[0]; // hostname for server
+        String UserID = args[2];// UserID for user
+        String Recepient = null;
+        Socket sock = new Socket(hostname, port);
+        DataOutputStream out = new DataOutputStream(sock.getOutputStream());
+        DataInputStream inp = new DataInputStream(sock.getInputStream());
+
+        Scanner sc = new Scanner(System.in);
+        String Line = null;
+        System.out.println(String.format("Client Program (user: %s)",UserID));
+        System.out.println("-------------------------------");
+        System.out.println("Do you want to send a message? [y/n]");
+        if (sc.nextLine().toLowerCase().equals(String.valueOf('y'))) {
+
+            System.out.println("Enter the recipient userid:");
+            Recepient = sc.nextLine();
+            while ((Line = sc.nextLine()) != null && !Line.equalsIgnoreCase("exit")) {
+
+
+                out.writeUTF(Line);
+                System.out.println(inp.readUTF());
+
+            }
+        }
+        else {
+
+        }
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,36 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+        int port = Integer.parseInt(args[0]);
+
+        ServerSocket servSock = new ServerSocket(port);
+        System.out.println("Waiting incoming connection...");
+
+        while(true) {
+            Socket sock = servSock.accept();
+            DataInputStream inp = new DataInputStream(sock.getInputStream());
+            DataOutputStream out = new DataOutputStream(sock.getOutputStream());
+
+            String x = null;
+
+            try {
+                while ((x = inp.readUTF()) != null) {
+
+                    System.out.println(x);
+                    out.writeUTF(x.toUpperCase());
+
+                }
+            }
+            catch(IOException e) {
+                System.err.println("Client closed its connection.");
+            }
+        }
+    }
+
+
 }
