611f2172c7c51e18bfd9fa8df86130de2facd074
lukey <ly105@student.le.ac.uk>
2024-02-20 03:08:42 +0000

    added encryption

--- src/Client.java
+++ src/Client.java
@@ -1,11 +1,39 @@
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.*;
 import java.net.Socket;
 import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.security.*;
+import java.security.spec.*;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Client {
+
+    static String toHex(byte[] input) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : input) sb.append(String.format("%02X", b));
+        return sb.toString();
+    }
+    static byte[] Encrypt(PublicKey key, String string) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.update(string.getBytes(StandardCharsets.UTF_8));
+    }
+    static String Decrypt(PrivateKey key, byte[]rawInput) throws UnsupportedEncodingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] stringBytes = cipher.update(rawInput);
+        return new String(stringBytes, StandardCharsets.UTF_8);
+    }
+
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         try {
 
@@ -19,8 +47,24 @@ public class Client {
             DataOutputStream out = new DataOutputStream(sock.getOutputStream());
             DataInputStream inp = new DataInputStream(sock.getInputStream());
             ObjectInputStream inpObj = new ObjectInputStream((sock.getInputStream()));
-            out.writeUTF(UserID);
 
+
+            //generate private key
+            String filepath = UserID + ".prv";
+            File f = new File(filepath);
+            byte[] keyBytes = Files.readAllBytes(f.toPath());
+            PKCS8EncodedKeySpec prvSpec = new PKCS8EncodedKeySpec(keyBytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            PrivateKey prvKey = kf.generatePrivate(prvSpec);
+
+
+            //hash userID
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            String hash = "gfhk2024:" + UserID;
+            byte[] d1 = md.digest(hash.getBytes());
+
+            hash = toHex(d1);
+            out.writeUTF(hash);
 
             Scanner sc = new Scanner(System.in);
             String Line = null;
@@ -42,7 +86,9 @@ public class Client {
                     for (Message userMessage : userMessages) {
 
                         System.out.printf("%n Date:  %s %n", userMessage.getTimestamp());
-                        System.out.printf(" Message:  %s %n%n", userMessage.getContent());
+                        System.out.println("Encrypted Bytes: " + new String(userMessage.getContent().getBytes(StandardCharsets.UTF_8), StandardCharsets.UTF_8));
+
+                        System.out.printf(" Message:  %s %n%n", Decrypt(prvKey,userMessage.getContent().getBytes(StandardCharsets.UTF_8)));
 
                     }
                     i++;
@@ -56,9 +102,31 @@ public class Client {
 
                 System.out.println("Enter the recipient userid:");
                 Recepient = sc.nextLine();
+
+                //generate public key
+                filepath = "server.pub";
+                System.out.println(filepath);
+                f = new File(filepath);
+                keyBytes = Files.readAllBytes(f.toPath());
+                X509EncodedKeySpec pubSpec = new X509EncodedKeySpec(keyBytes);
+                kf = KeyFactory.getInstance("RSA");
+                PublicKey pubKey = kf.generatePublic(pubSpec);
+
+                //hash Recepient
+                String rec = "gfhk2024:" + Recepient;
+                byte[] d2 = md.digest(rec.getBytes());
+                Recepient = toHex(d2);
+
                 out.writeUTF(Recepient);
+
+
                 System.out.println("Enter your message:");
-                out.writeUTF(sc.nextLine());
+                String message = sc.nextLine();
+
+
+
+                byte[] encrypted = Encrypt(pubKey,message);
+                out.writeUTF(encrypted.toString());
 
 
             }
@@ -66,17 +134,23 @@ public class Client {
 
 
 
-        catch(
-    SocketException e)
-
-    {
+        catch(SocketException e) {
         System.out.println("Lost Connection.");
-    } catch(
-    ClassNotFoundException e)
-
-    {
+    } catch(ClassNotFoundException e) {
         throw new RuntimeException(e);
-    }
+    } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeySpecException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchPaddingException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalBlockSizeException e) {
+            throw new RuntimeException(e);
+        } catch (BadPaddingException e) {
+            throw new RuntimeException(e);
+        } catch (InvalidKeyException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
 
--- src/Server.java
+++ src/Server.java
@@ -1,19 +1,46 @@
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.ArrayList;
 import java.util.List;
-import java.io.ObjectOutputStream;
 
 public class Server {
+    static byte[] Encrypt(PublicKey key, String string) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, key);
+        return cipher.update(string.getBytes(StandardCharsets.UTF_8));
+    }
+    static String Decrypt(PrivateKey key, byte[]rawInput) throws UnsupportedEncodingException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, key);
+        byte[] stringBytes = cipher.doFinal(rawInput);
+        return new String(stringBytes, StandardCharsets.UTF_8);
+    }
     public static List<Message> userMessages = new ArrayList<Message>();
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InvalidKeySpecException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
         int port = Integer.parseInt(args[0]);
 
         ServerSocket servSock = new ServerSocket(port);
         System.out.println("Server online...");
+
+        //generate private key
+        String filepath = "server.prv";
+        File f = new File(filepath);
+        byte[] keyBytes = Files.readAllBytes(f.toPath());
+        PKCS8EncodedKeySpec prvSpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        PrivateKey prvKey = kf.generatePrivate(prvSpec);
+
 
         while(true) {
             Socket sock = servSock.accept();
@@ -37,7 +64,8 @@ public class Server {
                 String Recepient = inp.readUTF();
                 System.out.printf("Sending Message to %s %n",Recepient);
                 String message = inp.readUTF();
-                System.out.printf("Message content: %s %n",message);
+                System.out.printf("Message content: %s %n",Decrypt(prvKey,message.getBytes(StandardCharsets.UTF_8)));
+
                 Message log = new Message(senderName, message, Recepient);
                 userMessages.add(log);
 
@@ -49,6 +77,10 @@ public class Server {
     }
 
     public static List<Message> CheckMessages(String UserID){
+        //decrypt UserId
+
+
+
         List<Message> myMessages = new ArrayList<Message>();
         for (Message message : userMessages){
 
